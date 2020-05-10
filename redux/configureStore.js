57d8fe2e5b584eab7c4db5eb9004882580409980
Oleg Mamiev <olegios12@gmail.com>
2020-05-10 11:23:25 +0300

    pesist redux store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
 
@@ -7,10 +7,19 @@ import {comments} from './comments';
 import {leaders} from './leaders';
 import {promotions} from './promotions';
 import {favorites} from './favorites';
+import {persistStore, persistCombineReducers} from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 
 export const ConfigureStore = () => {
+
+    const config = {
+        key: 'root',
+        storage,
+        debug: true,
+    };
+
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             dishes,
             comments,
             leaders,
@@ -20,5 +29,7 @@ export const ConfigureStore = () => {
         applyMiddleware(thunkMiddleware, logger)
     );
 
-    return store;
+    const persistor = persistStore(store);
+
+    return {persistor, store};
 };
